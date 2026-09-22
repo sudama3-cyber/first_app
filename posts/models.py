410f3aed8cc604614fd53f3c8db9b8c5344dd788
sudama3 <sudama3@gmail.com>
2026-09-23 00:52:06 +0700

    Initial commit

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+  class Meta:
+    db_table = 'posts'
+
+  content = models.CharField(blank=True)
+  created_at = models.DateTimeField(default=timezone.now)    
+
+# Create your models here.
